f24a5649885a5b035a109e74d67cf6000780589d
EJ Bensing <ebensing@rm-dash-r.com>
2013-08-23 16:53:18 -0700

    changes

--- shutdownSharding-small.sh
+++ shutdownSharding-small.sh
@@ -4,8 +4,8 @@ mongo admin shutdown.js --port 67017
 
 #shutdown the config servers
 mongo admin shutdown.js --port 57017
-mongo admin shutdown.js --port 57018
-mongo admin shutdown.js --port 57019
+#mongo admin shutdown.js --port 57018
+#mongo admin shutdown.js --port 57019
 
 #shutdown the sharding servers
 mongo admin shutdown.js --port 37017
@@ -17,6 +17,6 @@ sleep 2
 sudo rm -rf /data/s1
 sudo rm -rf /data/s2
 sudo rm -rf /data/cfg-a
-sudo rm -rf /data/cfg-b
-sudo rm -rf /data/cfg-c
+#sudo rm -rf /data/cfg-b
+#sudo rm -rf /data/cfg-c
 
